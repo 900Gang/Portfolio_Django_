a6d4d7303df3323058eafe75b776e0c2e118a7e9
900Gang <anandanand6776@gmail.com>
2026-09-15 23:11:49 +0530

    Prepare Django portfolio for production deployment

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+import dj_database_url
+
 from .env import get_bool, get_list, get_str, load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -100,12 +102,25 @@ WSGI_APPLICATION = 'portfolio_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# Database
+
+DATABASE_URL = get_str('DATABASE_URL', '')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=not DEBUG,
+        )
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
